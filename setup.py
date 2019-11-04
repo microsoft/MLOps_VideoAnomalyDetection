9ed3066a5de561393b6ddfe840bf95541dfdd5d3
David A. Hannasch <David.A.Hannasch@gmail.com>
2019-11-03 17:06:01 -0700

    Install PredNet as a dependency.

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ setup(
     ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     install_requires=[
+        'prednet @ git+https://github.com/dHannasch/prednet.git@add-small-video-test',
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
