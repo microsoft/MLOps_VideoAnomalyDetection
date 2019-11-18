3cb8cc7448aa74f9f1bd7988af7623ec2cf40d8e
David A. Hannasch <David.A.Hannasch@gmail.com>
2019-11-18 12:35:35 -0700

    import pytest

--- tests/test_video_anomaly_detection.py
+++ tests/test_video_anomaly_detection.py
@@ -1,11 +1,13 @@
 
+import pytest
+
 from video_anomaly_detection.cli import main
 
 
 def test_main():
     with pytest.raises(SystemExit):
-    try:
-        main(['--help'])
-    except SystemExit as e:
-        assert e.code == 0
-        raise
+        try:
+            main(['--help'])
+        except SystemExit as e:
+            assert e.code == 0
+            raise
